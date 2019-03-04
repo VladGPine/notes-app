af2e7cc8f0467c3e2a2a02353648f2166f0fdb20
Vladimir <shikarno_3000@inbox.ru>
2019-03-05 02:06:51 +0300

    basic styles for Note component has been added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Note from './Note'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Note />
       </div>
     );
   }
---
+++ src/Note.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import './note.css'
+
+class Note extends Component {
+  render() {
+    return (
+      <div className='note-container'>
+        <h3>Title</h3>
+        <p>Content Content Content Content Content Content Content </p>
+      </div>
+    )
+  }
+}
+
+export default Note

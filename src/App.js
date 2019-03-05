70398369ad10813e951500c00a8383d3399b5798
Vladimir <shikarno_3000@inbox.ru>
2019-03-05 22:38:06 +0300

    delete-btn animation has been added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,48 @@ import './App.css';
 import Note from './Note'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: 1,
+          title: 'Title',
+          content: 'Content content content content content'
+        },
+        {
+          id: 2,
+          title: 'Title',
+          content: 'Content content content content content'
+        },
+        {
+          id: 3,
+          title: 'Title',
+          content: 'Content content content content content'
+        },
+        {
+          id: 4,
+          title: 'Title',
+          content: 'Content content content content content'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Note />
+        {
+          this.state.notes.map(note => {
+            return (
+              <Note 
+                key={note.id}
+                title={note.title}
+                content={note.content} />
+            )
+         
+          })
+        }
       </div>
     );
   }
--- src/Note.jsx
+++ src/Note.jsx
@@ -3,11 +3,18 @@ import React, { Component } from 'react'
 import './note.css'
 
 class Note extends Component {
+  constructor(props) {
+    super(props);
+    this.title = props.title;
+    this.content = props.content;
+    this.id = props.id
+  }
+
   render() {
     return (
       <div className='note-container'>
-        <h3>Title</h3>
-        <p>Content Content Content Content Content Content Content </p>
+        <h3>{this.title}</h3>
+        <p>{this.content}</p>
         
           {/* <button className="edit-btn note-btn">Edit</button> */}
           <button className="delete-btn note-btn">

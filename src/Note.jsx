b9c191afc1958edb4109392b641dce3fd5e210d3
Vladimir <shikarno_3000@inbox.ru>
2019-03-05 03:23:03 +0300

    more styles for Note component

--- src/Note.jsx
+++ src/Note.jsx
@@ -8,6 +8,12 @@ class Note extends Component {
       <div className='note-container'>
         <h3>Title</h3>
         <p>Content Content Content Content Content Content Content </p>
+        
+          {/* <button className="edit-btn note-btn">Edit</button> */}
+          <button className="delete-btn note-btn">
+            <span></span>
+          </button>
+        
       </div>
     )
   }
